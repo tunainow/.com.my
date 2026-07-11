4785d690f7459f115b0ae3ff75eba1b9efb88b8c
tunainow <pokdikMaAbah@outlook.my>
2026-07-11 18:40:16 +0800

    add: Clock page

---
+++ src/pages/clock/page.tsx
@@ -0,0 +1,9 @@
+import DigitalClock from '@/components/DigitalClock';
+
+export default function ClockPage() {
+  return (
+    <div className="bg-background-950">
+      <DigitalClock />
+    </div>
+  );
+}
